63438e5a528787d27084f66ee56371c66aabf9e3
PD-PradhyumanShekhawat <spradhyumansingh@gmail.com>
2026-09-15 21:14:08 -0400

    Add worker shutdown handling

--- src/bitcoin_worker.erl
+++ src/bitcoin_worker.erl
@@ -1,17 +1,87 @@
 -module(bitcoin_worker).
 
--export([start/1]).
+-export([start/2]).
 
-start(BossPid) ->
-    spawn(fun() -> worker_loop(BossPid) end).
+start(BossPid, GatorLinkId) ->
+    spawn(fun() ->
+        worker_loop(BossPid, GatorLinkId)
+    end).
 
-worker_loop(BossPid) ->
+worker_loop(BossPid, GatorLinkId) ->
     receive
         {work, K, Start, End} ->
-            Results = bitcoin_miner:mine_range(K, Start, End),
-            BossPid ! {work_complete, self(), Start, End, Results},
-            worker_loop(BossPid)
+            Results =
+                bitcoin_miner:mine_range(
+                    K,
+                    Start,
+                    End,
+                    GatorLinkId
+                ),
+
+            BossPid ! {
+                work_complete,
+                self(),
+                Start,
+                End,
+                Results
+            },
+
+            worker_loop(
+                BossPid,
+                GatorLinkId
+            );
+
+        stop ->
+            ok
     end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% -module(bitcoin_worker).
+
+% -export([start/2]).
+
+% start(BossPid, GatorLinkId) ->
+%     spawn(fun() -> 
+%         worker_loop(BossPid, GatorLinkId) 
+%     end).
+
+% worker_loop(BossPid, GatorLinkId) ->
+%     receive
+%         {work, K, Start, End} ->
+%             Results =
+%                 bitcoin_miner:mine_range(                   %% The worker calles the bitcoin_miner, & performs calculation
+%                     K,
+%                     Start,
+%                     End,
+%                     GatorLinkId
+%                 ),
+
+%             BossPid ! {
+%                 work_complete,
+%                 self(),
+%                 Start,
+%                 End,
+%                 Results
+%             },
+
+%             worker_loop(BossPid, GatorLinkId)               %% The worker is not printing anything, it just sends the results to the Boss
+%     end.
 
 
 
